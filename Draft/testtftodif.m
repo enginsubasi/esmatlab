50c4c2f8b68693910d6562e677c14f74ffca3c0a
Engin <enginsubasi@gmail.com>
2022-09-14 22:23:01 +0300

    +

--- Draft/testtftodif.m
+++ Draft/testtftodif.m
@@ -9,15 +9,5 @@ syms s t                                                    % Invoke Symbolic Ma
 snum = poly2sym(num, s)                                     % Symbolic Numerator Polynomial
 sden = poly2sym(den, s)                                     % Symbolic Denominator Polynomial
 FT_time_domain = ilaplace(snum/sden)                        % Inverse Laplace Transform
-%FT_time_domain = simplify(FT_time_domain, 'Steps',10)       % Simplify To Get Nice Result
-%FT_time_domain = collect(FT_time_domain, exp(-t))           % Optional Further Factorization
-
-
-x=ones(1,100);
-%x=x*0.1;
-
-
-y=3*exp(-x).*(cos(2.*x) + (2.*sin(2.*x))./3);
-plot(y);
-
-
+FT_time_domain = simplify(FT_time_domain, 'Steps',10)       % Simplify To Get Nice Result
+FT_time_domain = collect(FT_time_domain, exp(-t))           % Optional Further Factorization
